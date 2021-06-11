1df5703c709f7f211c8c001906641df849ec8088
diebraga <diebraga0055@gmail.com>
2021-06-11 17:25:14 +0100

    render user when login

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,7 +1,8 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from 'next/router'
-import { setCookie } from "nookies";
+import { parseCookies, setCookie } from "nookies";
+import { api } from "../services/api";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -31,6 +32,18 @@ export function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user 
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data 
+
+        setUser({ email, permissions, roles })
+      })
+    }
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await axios.post(`http://localhost:3333/sessions`, {
@@ -44,12 +57,15 @@ export function AuthProvider({ children }: AuthProviderProps){
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: "/",
       });
+
       setCookie(null, "nextauth.refreshToken", refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: "/",
       });
 
       setUser({ email, permissions, roles })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       router.push('/dashboard')
       alert(JSON.stringify('Success', response.data))  
